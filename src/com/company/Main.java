635ac12094e962ebc5c94d0bf21c29723d138264
nilsJavaNoob <nils1968@gmail.com>
2016-04-29 22:13:27 +0300

    editing1

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//  code here
+
+    }
+}
---
+++ src/com/company/SomeClass.java
@@ -0,0 +1,7 @@
+package com.company;
+
+/**
+ * Created by n on 29.04.16.
+ */
+public class SomeClass {
+}
